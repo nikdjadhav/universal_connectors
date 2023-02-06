2634ec94e2b4287ee3b20fa0b1d1387c0594df02
nikita <nikita.jadhav@livestrongtechnologies.com>
2023-02-06 16:46:52 +0530

    used session

--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,73 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const userController = require('./controllers/user.controllers');
-// const userRoute = require('./routes/user.routes');
-// const userRoute = require('./routes/v1');
-// const prisma = require('./db/prisma');
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const userController = require("./controllers/user.controllers");
+// require("dotenv").config();
+// const session = require("express-session");
 
 const app = express();
 dotenv.config();
 const port = 4000;
 
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-}
-// app.configure(function() {
-//     app.use(allowCrossDomain);
-//     //some other code
-// }); 
+// const secret = process.env.SESSION_SECRET;
+// const store = new session.MemoryStore();
+// const protect = (req, res, next) => {
+//   const { authenticated } = req.session;
+
+//   if (!authenticated) {
+//     res.sendStatus(401);
+//   } else {
+//     next();
+//   }
+// };
+// app.use(
+//   session({
+//     secret,
+//     resave: false,
+//     saveUninitialized: false,
+//     store,
+//   })
+// );
+
+var allowCrossDomain = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+};
+
 app.use(allowCrossDomain);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
 const v1Router = express.Router();
-app.use('/v1', v1Router);
+app.use("/v1", v1Router);
 
 // *** routes ***
-v1Router.post('/', userController.createUser);
-v1Router.post('/login', userController.userLogin);
+v1Router.post("/", userController.createUser);
+v1Router.post("/login", userController.userLogin);
+
+// app.get("/v1/login", (req, res) => {
+//   const { authenticated } = req.session;
+
+//   console.log('rr',req.session.user);
+//   if (authenticated) {
+//     // req.session.authenticated = true;
+//     res.send("Already authenticated");
+    
+//   } else {
+//     res.send("Successfully authenticated");
+
+//   }
+// });
+// app.get("/v1/logout", protect, (req, res) => {
+//     console.log('rr',req.session.user);
+//   req.session.destroy(() => {
+//     res.send("Successfully logged out");
+//   });
+// });
 
 app.listen(port, () => {
-    console.log('Server is running on port ' + port);
+  console.log("Server is running on port " + port);
 });
