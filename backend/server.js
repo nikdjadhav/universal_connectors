f528d542ecd7948e9bfc0da78d0933a4bb265f9d
nikita <nikita.jadhav@livestrongtechnologies.com>
2023-05-05 12:12:38 +0530

    updated server. js file

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ const app = express();
 dotenv.config();
 const port = process.env.PORT || 4000;
 
+
 var allowCrossDomain = function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
